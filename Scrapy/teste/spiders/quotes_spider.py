12d4b763552697ccc1a82829d3ca73856e173041
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-05-27 12:35:48 +0100

    Start

---
+++ Scrapy/teste/spiders/quotes_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+# To Run: scrapy crawl quotes -o quotes.jl
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"
+    start_urls = [
+        'https://en.wikipedia.org/wiki/Main_Page',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('ul'):
+            yield {
+                'text': quote.css('li').getall(),
+            }
+
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page = response.urljoin(next_page)
+            yield scrapy.Request(next_page, callback=self.parse)
