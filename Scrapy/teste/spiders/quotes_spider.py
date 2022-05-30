611bac6676631152ecb232bef93ddbc6d9b0d519
CVB-999999999 <77160567+CVB-999999999@users.noreply.github.com>
2022-05-30 19:14:16 +0100

    Crawler Done

--- Scrapy/teste/spiders/quotes_spider.py
+++ Scrapy/teste/spiders/quotes_spider.py
@@ -10,15 +10,46 @@ class QuotesSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
-        for quote in response.css('td'):
 
-            for li in quote.css('ul'):
+        links = []
 
-                yield {
-                    'text': li.css('li').getall(),
-                }
 
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
+        # Buscar os dados da pagina inicial
+        if str(response) == '<200 https://en.wikipedia.org/wiki/Main_Page>':
+
+            for quote in response.css('td')[2:-2]:
+                for li in quote.css('ul')[:3]:
+
+                    yield {
+                        'links': li.css('b').css('a::attr(href)').getall(),
+                        'articles': li.css('li').getall(),
+                    }
+                
+                    links = li.css('b').css('a::attr(href)').getall()
+                    articles = li.css('li').getall()
+
+                    break
+
+        # Buscar o resumo do acontecimento
+        else:
+            for quote in response.css('div.mw-parser-output'):
+                for div in quote.css('p')[1:]:
+
+                    yield {
+                        'resumo': div.get(),
+                    }
+                
+                    resumo = div.get()
+
+                    print('sjkbdsakjdkbjsa' + resumo)
+
+                    break
+
+        for a in links:
+            next_page = 'https://en.wikipedia.org' + a
+
+            print('-> ' + next_page)
+
+            if next_page is not None:
+                next_page = response.urljoin(next_page)
+                yield scrapy.Request(next_page, callback=self.parse)
